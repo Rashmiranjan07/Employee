12f6c78a0b0e8de94e3c68d8d64410028927b1dc
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-02-02 18:41:14 +0530

    delete add

---
+++ src/main/java/com/qsp/container2/DeleteEmployeeServlet.java
@@ -0,0 +1,31 @@
+package com.qsp.container2;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.qsp.repository2.EmployeeRepository;
+
+@WebServlet("/")
+public class DeleteEmployeeServlet {
+
+	private EmployeeRepository employeerepo = EmployeeRepository.getInstance();
+	
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter("email");
+		employeerepo.deleteEmployeeByEmail(email);
+		RequestDispatcher rd = req.getRequestDispatcher("");
+		rd.forward(req, resp);
+		
+	}
+	
+	}
+
+
+
+
+
